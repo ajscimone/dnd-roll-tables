9cc60862d9195a90dd8476c9d7a3f66db904d881
Joseph Beach <joseph.n.beach@outlook.com>
2023-04-30 21:14:40 -0500

    First 'working' version. Loot button generates loot in the console, but does not display the loot on the page yet.

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { CreateLootTables, GetLoot, GetRarity } from "../util/lootTables";
+import { CreateLootTables, GetLoot, GetRarities } from "../util/lootTables";
 import { Container, Col, Row, ToggleButtonGroup, ToggleButton, ListGroup, Button, ButtonGroup, Toast } from "react-bootstrap";
 
 function Loot(props) {
-    const [minorOrMajor, setMinorOrMajor] = useState("Minor");
+    const [minorOrMajor, setMinorOrMajor] = useState("Minor Items");
     const [rarity, setRarity] = useState("Common");
     const [rarities, setRarities] = useState([]);
     const [loot, setLoot] = useState("");
 
     useEffect(() => {
-        // setMinorOrMajor(GetLoot())
-        // setRarities(GetRarity(rarity))
-        // CreateLootTables()
+        // setMinorOrMajor()
+        setRarities(GetRarities(minorOrMajor))
+        CreateLootTables()
     }, [rarity]);
 
     const handleButtonClick = (event) => {
-        let currentRarity = GetRarity(minorOrMajor)
+        let currentRarity = GetRarities(minorOrMajor)
         if (currentRarity.includes(rarity)) {
             let loot = GetLoot(minorOrMajor, rarity)
             setLoot(loot)
+
         }
+        // console.log(minorOrMajor)
+        // console.log(rarity)
+        // console.log(rarities)
+        console.log(loot)
     };
 
     return (
@@ -32,22 +37,24 @@ function Loot(props) {
                             <ListGroup>
                                 <ListGroup.Item>
                                     <h3>Minor or Major Items</h3>
-                                    <ToggleButtonGroup type="radio" name="options" defaultValue={minorOrMajor}>
+                                    <ToggleButtonGroup type="radio" name="minorOrMajorRadioButtons" defaultValue={minorOrMajor}>
                                         <ToggleButton
                                             key={1}
                                             id={"minorOrMajor-radio-1"}
                                             variant={"secondary"}
+                                            value={"Minor Items"}
                                             onChange={(e) => setMinorOrMajor(e.currentTarget.value)}
                                         >
-                                            Minor
+                                            Minor Items
                                         </ToggleButton>
                                         <ToggleButton
                                             key={2}
                                             id={"minorOrMajor-radio-2"}
                                             variant={"secondary"}
+                                            value={"Major Items"}
                                             onChange={(e) => setMinorOrMajor(e.currentTarget.value)}
                                         >
-                                            Major
+                                            Major Items
                                         </ToggleButton>
                                     </ToggleButtonGroup>
                                 </ListGroup.Item>
@@ -57,7 +64,7 @@ function Loot(props) {
                             <ListGroup>
                                 <ListGroup.Item>
                                     <h3>Rarity</h3>
-                                    <ToggleButtonGroup type="radio" name="options" defaultValue={rarity}>
+                                    <ToggleButtonGroup type="radio" name="RarityRadioButtons" defaultValue={rarity}>
                                         {rarities.map((thisRarity, idx) => (
                                             <ToggleButton
                                                 key={idx}
--- src/util/lootTables.js
+++ src/util/lootTables.js
@@ -4,54 +4,22 @@ import replaceStringRoll from "../util/replaceStringDiceRoll"
 import LootData from "../data/xanthars-magic-item-tables.json";
 
 let LootMap = new Map()
-let MinorOrMajor = ["Minor Items", "Major Items"]
 
-function parseLoot(loot) {
-    return loot
-}
-
-function createTableEntryFromRange(sourceJsonFile) {
-    const range = sourceJsonFile.range.split("-");
-    if (range.length === 2) {
-        let subTable = []
-        for (let i = parseInt(range[0]); i <= parseInt(range[1]); ++i) {
-            subTable.push(parseLoot(sourceJsonFile.event))
-        }
-        return subTable
-    } else if (range.length) {
-        return [parseLoot(sourceJsonFile.event)]
-    } else {
-        return null;
-    }
-}
-
-function createTableEntries(inputArray) {
-    let rollTable = []
-    inputArray.forEach((row) => {
-        let expandedSubTable = createTableEntryFromRange(row)
-        rollTable = rollTable.concat(expandedSubTable)
-    });
-    return rollTable
-}
-
-function CreateLootTables() { //THIS IS WHAT YOU WERE LAST WORKING ON
+function CreateLootTables() {
     for (let [minorOrMajor, rarities] of Object.entries(LootData)) {
         let minorOrMajorMap = new Map()
-        for (let [rarity, loot] of Object.entries(rarities)) {
-            minorOrMajorMap.set(rarity, createTableEntries(loot))
+        for (let [rarity, items] of Object.entries(rarities)) {
+            minorOrMajorMap.set(rarity, items)
         }
         LootMap.set(minorOrMajor, minorOrMajorMap)
     }
 }
 
 function GetLoot(minorOrMajorSelection, raritySelection) {
-    let loot = randomArrayEntry(LootMap.get(minorOrMajorSelection).get(raritySelection))
-    let calculatedLoot = replaceStringRoll(loot)
-    return calculatedLoot
-
+    return randomArrayEntry(LootMap.get(minorOrMajorSelection).get(raritySelection))
 }
 
-function GetRarity(minorOrMajorSelection) {
+function GetRarities(minorOrMajorSelection) {
     let arrayOfRarities = []
     for (let [minorOrMajor, rarities] of Object.entries(LootData)) {
         if (minorOrMajorSelection == minorOrMajor) {
@@ -65,4 +33,4 @@ function GetRarity(minorOrMajorSelection) {
 
 
 
-export { CreateLootTables, GetLoot, GetRarity };
+export { CreateLootTables, GetLoot, GetRarities };
